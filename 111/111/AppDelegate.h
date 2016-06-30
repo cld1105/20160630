9bdf79ced9ce0863e54a17d025d5e3141da80895
ChenLD <ChenLD@163.com>
2016-06-30 16:39:13 -0700

    初始化

---
+++ 111/111/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  111
+//
+//  Created by mac on 16/6/30.
+//  Copyright © 2016年 ChenLD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 111/111/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  111
+//
+//  Created by mac on 16/6/30.
+//  Copyright © 2016年 ChenLD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
